6357dc004d7294c71d84e23936a7ea946d55e986
0x00-pl <0x00.pl@gmail.com>
2018-07-10 20:44:39 +0800

    add function decl

---
+++ sound_ifft.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = sound_ifft(inputArg1, inputArg2)
+%SOUND_IFFT Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
